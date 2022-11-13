e9e844ea04f22cd04058297e00d52d974a8e327a
mdarora <mdarora0001@gmail.com>
2022-11-13 15:23:09 -0500

    created new class for category table

---
+++ MohitsBooks.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MohitsBooks.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name="Category Name")]
+        public string Name { get; set; }
+    }
+}
