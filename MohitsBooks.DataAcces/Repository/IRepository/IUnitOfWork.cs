41a7113b0b8c6d90e0fb763ab26bada60ac6fe0b
mdarora <mdarora0001@gmail.com>
2022-11-14 01:11:35 -0500

    Accesser modifier removed

--- MohitsBooks.DataAcces/Repository/IRepository/IUnitOfWork.cs
+++ MohitsBooks.DataAcces/Repository/IRepository/IUnitOfWork.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace MohitsBooks.DataAcces.Repository.IRepository
 {
 
-    public interface IUnitOfWork : IDisposable
+    interface IUnitOfWork : IDisposable
     {
         ICategoryRepository Category { get; }
         ISP_Call SP_Call { get; }
