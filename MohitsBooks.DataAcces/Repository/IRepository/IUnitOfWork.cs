096096e46784ad52e006f40a1a136a3c43249bc7
mdarora <mdarora0001@gmail.com>
2022-11-14 00:03:29 -0500

    created new interface and its class

---
+++ MohitsBooks.DataAcces/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MohitsBooks.DataAcces.Repository.IRepository
+{
+
+    interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+        ISP_Call SP_Call { get; }
+    }
+}
---
+++ MohitsBooks.DataAcces/Repository/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using MohitsBooks.DataAcces.Repository.IRepository;
+using MohitsBookStore.DataAcces.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MohitsBooks.DataAcces.Repository
+{
+    public class UnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            SP_Call = new SP_Call(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+        public SP_Call SP_Call { get; private set; }
+    }
+}
