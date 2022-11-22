a1e2553c5bdef4db30ea8913dcc2b151e8ddbb7c
mdarora <mdarora0001@gmail.com>
2022-11-21 19:58:28 -0500

    new CoverType repo class

---
+++ MohitsBooks.DataAcces/Repository/CoverTypeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using MohitsBooks.Models;
+using MohitsBooks.DataAcces.Repository.IRepository;
+using MohitsBookStore.DataAccess.Data;
+
+namespace MohitsBooks.DataAcces.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+        }
+        public void Update(CoverType coverType)
+        {
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+            if(objFromDb != null)
+            {
+                objFromDb.Name = coverType.Name;
+            }
+        }
+    }
+}
