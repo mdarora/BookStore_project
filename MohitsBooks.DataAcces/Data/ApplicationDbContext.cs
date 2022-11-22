d4a5b176252c91aba5b8a0f87dab8850823d5119
mdarora <mdarora0001@gmail.com>
2022-11-21 23:49:11 -0500

    added reference for nre product model class

--- MohitsBooks.DataAcces/Data/ApplicationDbContext.cs
+++ MohitsBooks.DataAcces/Data/ApplicationDbContext.cs
@@ -15,5 +15,6 @@ namespace MohitsBookStore.DataAccess.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
